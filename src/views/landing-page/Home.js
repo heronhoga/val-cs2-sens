c0c3e05a4453038e4179359b61cf90cc55887ff3
heronhoga <nozzeravixer@gmail.com>
2024-02-16 18:04:51 +0700

    fix:fixing color and shape of the button

--- src/views/landing-page/Home.js
+++ src/views/landing-page/Home.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="containerHome">
-      <div className="leftHome"><Link to="/cs2-to-val"><button className='csToValBtn'>Convert CS2 Sensitivity to Valorant</button></Link></div>
-      <div className="rightHome"><Link to="/val-to-cs2"><button className='valToCsBtn'>Convert Valorant Sensitivity to CS2</button></Link></div>
+      <div className="leftHome">
+        <img src="/public/val.jpg" alt="Valorant" />
+        <Link to="/cs2-to-val">
+          <button className="csToValBtn">
+            Convert CS2 Sensitivity to Valorant
+          </button>
+        </Link>
+      </div>
+      <div className="rightHome">
+        <Link to="/val-to-cs2">
+          <button className="valToCsBtn">
+            Convert Valorant Sensitivity to CS2
+          </button>
+        </Link>
+        <img src="/public/cs2.jpeg" alt="CS2" />
+      </div>
     </div>
   );
 }
